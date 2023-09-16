b2ab92cafffee52779ee3b9ca21dfa4bdbe22afd
naccaratocarolina <naccaratocarolina@dcc.ufrj.br>
2023-09-15 21:56:07 -0300

    New trial

--- mm1.py
+++ mm1.py
@@ -1,69 +1,44 @@
-import math
+from queue import Queue
 import random
-from collections import deque
+import math
 
 class Event():
-  def __init__(self, type, time):
-    self._type = type
-    self._time = time
+  def __init__(self, id, type, time):
+    self._id = id # Identificador do evento
+    self._type = type # Chegada (arrival) ou partida (departure)
+    self._time = time # Tempo de ocorrência do evento
+  
+  @property
+  def id(self):
+    return self._id
 
   @property
   def type(self):
     return self._type
-
+  
   @property
   def time(self):
     return self._time
 
-  def __repr__(self):
-    return '(%s, %f)' % (self.type, self.time)
-
-class Queue():
-  def __init__(self):
-    self._queue = [] # Lista de eventos
-    self._client = 0 # Numero de clientes
-  
-  @property
-  def queue(self):
-    return self._queue
-
-  @property
-  def client(self):
-    return self._client
-  
-  def enqueue(self, event):
-    self._client += 1
-    # Inserir evento na lista de eventos
-    self._queue.append(event)
-    # Ordenar lista de eventos
-    self._queue.sort(key=lambda x: x.time)
-    # Retornar numero do cliente
-    return self._client
-  
-  def dequeue(self):
-    # Retirar primeiro evento da lista de eventos
-    self._queue.pop(0)
-    # Retornar numero do cliente atendido
-    return self._client
-  
-  def is_empty(self):
-    # Verificar se lista de eventos está vazia
-    return len(self._queue) == 0
-
-  def __repr__(self):
-    return str(self._queue)
-
-class MM1():
-  def __init__(self, server_rate, arrival_rate, DEBUG = False):
-    self.DEBUG = DEBUG # Inicializar variável de debug
-    self.N = 0 # Inicializar variavel de estado
-    self.L = [] # Inicializar lista de eventos
-    self.queue = Queue() # Cliente no servidor
+class mm1():
+  def __init__(self, server_rate, arrival_rate, max_size = 100):
     self.server_rate = server_rate # Taxa de serviço do servidor
     self.arrival_rate = arrival_rate # Taxa de chegada de clientes
-    self.clock = 0 # Inicializar relógio do simulador (eventos/time unit)
+    self.max_size = max_size # Número máximo de chegadas
+    self.N = 0 # Número de clientes na fila
+    self.total_arrivals = 0
+    self.L = Queue(max_size) # Lista de eventos
+    self.clock = 0 # Relógio do simulador (eventos/time unit)
     self.next_arrival = self.generate_arrival() # Sortear tempo da próxima chegada
     self.next_departure = math.inf # Inicializar tempo da próxima partida com infinito
+  
+  def print_queue(self):
+    if self.L.empty():
+      print('[]')
+    else:
+      for event in self.L.queue:
+        print(f'#{event.id}', end = ' ')
+    print()
   
   def generate_arrival(self):
     # Sortear tempo da próxima chegada de acordo com distribuição exponencial
@@ -85,104 +60,72 @@ class MM1():
     self.clock += self.next_arrival
 
     # Novo evento de chegada
-    arrival_event = Event('arrival', self.clock)
+    self.total_arrivals += 1
+    event = Event(self.total_arrivals, 'arrival', self.clock)
 
     # Incluir evento na lista L
-    self.L.append(arrival_event)
+    if not self.L.full():
+      self.L.put(event)
+    else:
+      print('Fila cheia!')
 
-    # Incluir cliente na fila
-    arrived = self.queue.enqueue(arrival_event)
+    # Printa log de chegada
+    print('Cliente #%d chegou no instante %f' % (event.id, self.clock))
 
-    if self.DEBUG:
-      print('Cliente #%d chegou no instante %f' % (arrived, self.clock))
+    # Imprime fila
+    self.print_queue()
 
     # Se N = 1, cliente passa a ser atendido imediatamente
-    if self.N == 1:
-      # Sortear tempo de partida
-      self.next_departure = self.generate_departure()
-
-      # Atualizar relógio do simulador
-      self.clock += self.next_departure
-
-      # Incluir evento na lista L
-      self.L.append(Event('departure', self.clock))
-
-      # Retirar cliente da fila
-      served = self.queue.dequeue()
-
-      if self.DEBUG:
-        print('Cliente #%d atendido no instante %f' % (served, self.clock))
-
-  # Handler para evento de partida
-  def departure(self):
+    #if self.N == 1:
+      #self.departure(True)
+  
+  def departure(self, force = False):
     # Decrementar número de clientes na fila
     self.N -= 1
 
-    # Se N > 0, cliente no início da fila passa a ser atendido
-    if self.N > 0:
+    if self.N > 0 or force:
       # Sortear tempo de partida
       self.next_departure = self.generate_departure()
 
       # Atualizar relógio do simulador
       self.clock += self.next_departure
 
-      # Incluir evento na lista L
-      self.L.append(Event('departure', self.clock))
+      # Retira cliente da fila
+      event = self.L.get()
 
-      # Retirar cliente da fila
-      served = self.queue.dequeue()
+      # Novo evento de partida
+      event = Event(event.id, 'departure', self.clock)
 
-      if self.DEBUG:
-        print('Cliente #%d atendido no instante %f' % (served, self.clock))
-
-class Simulator():
-  def __init__(self, arrival_rate, server_rate, max_events, DEBUG=False):
-    self.DEBUG = DEBUG
-    self.arrival_rate = arrival_rate
-    self.server_rate = server_rate
-    self.max_events = max_events
+      # Printa log de partida
+      print('Cliente #%d atendido no instante %f' % (event.id, self.clock))
+    else:
+      print('Servidor ocioso no instante %f' % (self.clock))
   
-  def run(self):
-    simulator = MM1(self.server_rate, self.arrival_rate, self.DEBUG)
-
-    # Evento inicial: Chegada de cliente
-    simulator.arrival()
-
-    # Quantidade de eventos processados
-    processed_events = 0
-
-    # Loop principal do simulador
-    while simulator.L and processed_events < self.max_events:
-      # Retirar primeiro evento da lista e tratar evento
-      e = simulator.L.pop(0)
-      processed_events += 1
-
-      if simulator.DEBUG:
-        print('Evento do tipo %s no instante %f' % (e.type, e.time))
-
-      # Se evento for do tipo chegada, tratar chegada
-      if e.type == 'arrival':
-        simulator.arrival()
-      # Se evento for do tipo partida, tratar partida
-      elif e.type == 'departure':
-        simulator.departure()
-    
-    return simulator.clock
-
-  def little_law(self, arrival_rate, server_rate):
-    return arrival_rate/(server_rate - arrival_rate)
-
 # Plano de controle: Loop principal do simulador
 if __name__ == '__main__':
-  server_rate = 0.6  # Taxa de serviço do servidor
-  arrival_rate = 0.3 # Taxa de chegada de clientes
-  max_events = 10 # Número máximo de eventos a serem processados
+  server_rate = 1.0 # Taxa de serviço do servidor
+  arrival_rate = 0.5 # Taxa de chegada de clientes
+  max_arrivals = 10 # Número máximo de chegadas
 
   # Inicializar simulador
-  simulator = Simulator(arrival_rate, server_rate, max_events, True)
+  simulator = mm1(server_rate, arrival_rate, max_arrivals)
 
-  # Executar simulador
-  clock = simulator.run()
+  # Evento inicial de chegada
+  simulator.arrival()
 
-  # Imprimir tempo de simulação
-  print('Tempo de simulação: %f' % clock)
+  # Quantidade de eventos atendidos
+  processed_events = 0
+
+  while simulator.L.qsize() > 0 and processed_events < max_arrivals:
+    # Incrementar quantidade de eventos atendidos
+    processed_events += 1
+
+    # Obter próximo evento
+    event = simulator.L.get()
+
+    # Chegada
+    if event.type == 'arrival':
+      simulator.arrival()
+    # Partida
+    elif event.type == 'departure':
+      simulator.departure()
