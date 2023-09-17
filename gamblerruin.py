5fc3e664842921d7eba20d92b053a3ae82d9cc47
naccaratocarolina <naccaratocarolina@dcc.ufrj.br>
2023-09-17 15:46:50 -0300

    Fixing prints

--- gamblerruin.py
+++ gamblerruin.py
@@ -20,7 +20,6 @@ class Gambler:
   def bet(self):
     # Sortear resultado da aposta
     bet = random.choices([True, False], weights=[self.win_prob, self.loss_prob])[0]
-    gambler.gambler_log(bet)
     
     # Atualizar balanço
     if bet:
@@ -34,24 +33,37 @@ class Gambler:
 
     # Adiciona balanço atual à lista de balanços
     self.capital_log.append((self.rounds, self.capital))
+
+    gambler.gambler_log(bet)
   
   def gambler_log(self, won_round):
     if won_round:
-      print(f'{WIN_COLOR}Rodada {self.rounds} | Ganhou {self.bet_amount} | Balanço: {self.capital+self.bet_amount}{RESET_COLOR}')
+      print(f'{WIN_COLOR}Rodada {self.rounds} | Ganhou {self.bet_amount} | Balanço: {self.capital}{RESET_COLOR}')
     else:
-      print(f'{LOSS_COLOR}Rodada {self.rounds} | Perdeu {self.bet_amount} | Balanço: {self.capital-self.bet_amount}{RESET_COLOR}')
+      print(f'{LOSS_COLOR}Rodada {self.rounds} | Perdeu {self.bet_amount} | Balanço: {self.capital}{RESET_COLOR}')
 
 def run(env, gambler, goal):
-  while gambler.capital > 0 and gambler.capital <= goal:
+  while gambler.capital > 0 and gambler.capital < goal:
     yield env.process(gambler.bet())
 
 if __name__ == "__main__":
   initial_capital = 1 # Balanço inicial
   bet_amount = 1 # Valor da aposta
-  win_prob = 0.5 # Probabilidade de ganhar
+  win_prob = 0.51 # Probabilidade de ganhar
   goal = 4 # Objetivo
+
+  # Imprime quantidade inicial, objetivo e valor da aposta
+  print(f'Capital inicial: {initial_capital}')
+  print(f'Objetivo: {goal}')
+  print(f'Valor da aposta: {bet_amount}\n')
 
   env = simpy.Environment() # Cria ambiente de simulação
   gambler = Gambler(env, initial_capital, bet_amount, win_prob) # Cria jogador
   env.process(run(env, gambler, goal)) # Adiciona jogador ao ambiente de simulação
   env.run() # Roda a simulação
+
+  # Imprime se houve ruina ou não
+  if gambler.capital == 0:
+    print(f'\nO jogador faliu')
+  elif gambler.capital == goal:
+    print(f'\nO jogador atingiu o objetivo')
