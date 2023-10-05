144f4fc1337beff9cb66b106787f7d68ba147310
naccaratocarolina <naccaratocarolina@dcc.ufrj.br>
2023-10-04 22:20:25 -0300

    Adicionando primeira metrica (tempo de espera)

--- mm1.py
+++ mm1.py
@@ -7,7 +7,13 @@ GREEN = '\033[92m'
 RED = '\033[91m'
 YELLOW = '\033[93m'
 ORANGE = '\033[33m'
+WHITE = '\033[37m'
+BLACK_BG = '\033[40m'
 END = '\033[0m'
+
+def print_message(msg, color, clock, verbose):
+  if verbose:
+    print(f'{color}{clock:.2f}: {msg}{END}')
 
 # ================================
 # Evento
@@ -34,11 +40,12 @@ def generate_next_departure(service_rate):
 def print_queue(L):
   print(f'[{", ".join([str(event) for event in L])}]')
 
-def mm1_simulation(arrival_rate, service_rate, max_iter, max_queue_len):
+def mm1_simulation(arrival_rate, service_rate, max_iter, max_queue_len, verbose=True):
   L = [] # Lista de eventos (Fila de prioridades)
   N = 0 # Número de clientes na fila (variavel de estado)
   clock = 0
   customer_number = 0 
+  total_wait_time = 0 # Métrica importante para estatísticas
 
   def schedule_event(type, timestamp):
     nonlocal customer_number  # Permite acesso à variável customer_number declarada fora da função
@@ -55,6 +62,7 @@ def mm1_simulation(arrival_rate, service_rate, max_iter, max_queue_len):
   is_busy = True
   while len(L) > 0 and max_iter > 0:
     e = L.pop(0) # Remove evento e da fila (de prioridades) de eventos
+    wait_time = e.timestamp - clock # Calcula tempo de espera
     clock = e.timestamp # Atualiza relógio
 
     # Sortear tempo da próxima chegada e agendar evento
@@ -63,34 +71,37 @@ def mm1_simulation(arrival_rate, service_rate, max_iter, max_queue_len):
       if max_queue_len == -1 or N < max_queue_len:
         schedule_event('Arrival', clock + generate_next_arrival(arrival_rate))
         N += 1
-        print(f'{GREEN}{clock:.2f}: Cliente chega (N = {N}){END}')
+        print_message(f'Cliente chega (N = {N})', GREEN, clock, verbose)
         if N == 1:
           schedule_event('Departure', clock + generate_next_departure(service_rate))
       else:
-        print(f'{RED}{clock:.2f}: Cliente desiste (N = {N}){END}')
+        print_message(f'Cliente chega e desiste (N = {N})', RED, clock, verbose)
 
     # Se N > 0, sortear tempo da próxima partida e agendar partida
     if e.type == 'Departure':
       if N > 0:
+        total_wait_time += wait_time # Atualiza métrica
         # Se servidor estava ocioso, servidor volta a atender
         if not is_busy:
-          print(f'{CYAN}{clock:.2f}: Servidor volta a atender (N = {N}){END}')
+          print_message(f'Servidor volta a atender (N = {N})', CYAN, clock, verbose)
         N -= 1
         is_busy = True
         schedule_event('Departure', clock + generate_next_departure(service_rate))
-        print(f'{YELLOW}{clock:.2f}: Cliente parte (N = {N}){END}')
+        print_message(f'Cliente sai (N = {N})', YELLOW, clock, verbose)
 
     # Se N = 0 e servidor estava ocupado, servidor fica ocioso
     if N == 0 and is_busy:
-      print(f'{BLUE}{clock:.2f}: Servidor ocioso (N = {N}){END}')
+      print_message(f'Servidor fica ocioso (N = {N})', BLUE, clock, verbose)
       is_busy = False
 
     max_iter -= 1 # Decrementa variável de controle
+  
+  return total_wait_time, customer_number # Retorna métrica e número de clientes
 
-def mm1(arrival_rate, service_rate, max_iter, max_queue_len, iterations=2):
+def mm1(arrival_rate, service_rate, max_iter, max_queue_len, i=2):
   counter = 0
-  while counter < iterations:
-    print(f'{ORANGE}Simulação #{counter+1}{END}')
+  while counter < i:
+    print(f'{BLACK_BG}{WHITE}Simulação #{counter+1}{END}')
     mm1_simulation(arrival_rate, service_rate, max_iter, max_queue_len)
     print()
     counter += 1
@@ -105,7 +116,7 @@ def generate_next_event(probability, timestamp):
   else:
     return Event('Lose', timestamp, -1)
 
-def gambler_ruin(probability, goal, starting_amount):
+def gambler_ruin(probability, goal, starting_amount, verbose=True):
   amount = starting_amount # Equivalente a primeira chegada
   round = 0 # Número de rodadas ("clock")
 
@@ -115,30 +126,51 @@ def gambler_ruin(probability, goal, starting_amount):
     event = generate_next_event(probability, round)
     amount += event.id
 
-    # 
     if event.type == 'Win':
-      print(f'{PURPLE}{round}: Jogador ganhou {event.id} | Montante: {amount}{END}')
+      print_message(f'{round}: Jogador ganhou {event.id} | Montante: {amount}', PURPLE, round, verbose)
     
     if event.type == 'Lose':
-      print(f'{BLUE}{round}: Jogador perdeu {event.id*-1} | Montante: {amount}{END}')
+      print_message(f'{round}: Jogador perdeu {event.id} | Montante: {amount}', BLUE, round, verbose)
 
   # Imprime resultado
   if amount == goal:
-    print(f'O apostador atingiu o objetivo de {goal} em {round} rodadas')
+    print_message(f'O apostador atingiu o objetivo de {goal} em {round} rodadas', GREEN, round, verbose)
   else:
-    print(f'O apostador faliu em {round} rodadas')
+    print_message(f'O apostador faliu em {round} rodadas', RED, round, verbose)
+  
+  return round # Retorna número de rodadas
 
-def gambler(win_probability, goal, starting_amount, iterations=2):
+def gambler(win_probability, goal, starting_amount, i=2):
   counter = 0
-  while counter < iterations:
-    print(f'{ORANGE}Simulação #{counter+1}{END}')
+  while counter < i:
+    print(f'{BLACK_BG}{WHITE}Simulação #{counter+1}{END}')
     gambler_ruin(win_probability, goal, starting_amount)
     print()
     counter += 1
 
+# ================================
+# Estatísticas
+# ================================
+
+# O sistema é simulado por i iterações. Cada iteração possui K clientes.
+# Seja W_i_k o tempo de espera do k-ésimo cliente na i-ésima iteração,
+# o tempo de espera médio do k-ésimo cliente é W_i = Sum_k W_i_k / K
+# Quanto maior K, mais preciso é o resultado
+def wait_time(i, func, *args):
+  avg_wait_times = [] # Lista com tempos médios de cada iteração i
+  
+  for _ in range(i):
+    total_wait_time, k = func(*args) # Executa simulação
+    avg_wait_time = total_wait_time / k
+    avg_wait_times.append(avg_wait_time)
+  
+  return avg_wait_times
+
 def main():
-  mm1(0.5, 1, 5, -1, 4)
-  print()
+  result = wait_time(10, mm1_simulation, 0.5, 1, 10, -1, False)
+  print(result)
+  # mm1(0.5, 1, 10, -1, 1)
+  # print()
   gambler(0.5, 5, 2, 4)
 
 if __name__ == '__main__':
