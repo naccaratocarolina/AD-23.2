ccc9f2cb60fe4408a0ff44981e201e72d66546ae
naccaratocarolina <naccaratocarolina@dcc.ufrj.br>
2023-11-10 17:48:31 -0300

    Alterando geracao de filhos

--- T2/mm1_epidemy.py
+++ T2/mm1_epidemy.py
@@ -24,7 +24,13 @@ def run_epidemy(N, infection_rate, recovery_rate):
   tot_mu = 0  # Total dos tempos de recuperação
 
   generations = {} # Dicionário de gerações: {geração: [tempos de infecção (chegada) de cada indivíduo da mesma geração]}
-  n_gens = 0 # Contador de gerações
+  children = {} # Dicionário de filhos por geração: {geração: [número de filhos gerados por cada pai pertencente a mesma geração]}
+  n_gens = 1 # Contador de gerações
+  n_offspring = 0 # Variavel auxiliar para troca de geração
+
+  # Gera o tempo do paciente 0 que chegou no tempo 0 e gerou 0 filhos
+  generations[0] = [0]
+  children[0] = [0]
 
   # Gera o tempo da primeira infecção
   rand = random.random()
@@ -32,17 +38,12 @@ def run_epidemy(N, infection_rate, recovery_rate):
   tot_lambda = t_next_infection
 
   # Inicio da simulacao
-  # Instruções para a geração da distribuição offspring:
-  # Dada uma chegada (infecção), é gerado o tempo de recuperação do indivíduo e,
-  # a partir desse tempo, é gerado o número de indivíduos que foram infectados por
-  # ele durante o seu tempo de recuperação
-  # O critério de parada é quando o tempo de recuperação gerado for maior que
-  # o tempo de chegada (infecção) gerado. Todos os indivíduos que foram gerados por
-  # essa função correspondem a uma geração.
   it = 0
+  # while it < 10:
   while n_gens < N:
-  # while it < N:
+  # while True:
     it += 1
+
     # Evento de infecção
     if t_next_infection < t_next_recovery:
       # Atualiza relogio e contadores
@@ -56,17 +57,17 @@ def run_epidemy(N, infection_rate, recovery_rate):
       t_next_infection = clock + t_lambda
       tot_lambda += t_lambda
 
+      # Adiciona tempo de infecção/chegada
+      if n_gens not in generations:
+        generations[n_gens] = []
+      generations[n_gens].append(t_next_infection)
+
       # Chegada a um sistema vazio
-      if n == 1:
+      if n == 1 or t_next_recovery == INF:
         rand = random.random()
         t_mu = -math.log(rand) / recovery_rate
         t_next_recovery = clock + t_mu
         tot_mu += t_mu
-      
-      # Inicializa a primeira geração com o paciente 0 que chega no tempo 0
-      if n_gens == 0:
-        generations[n_gens] = [0]
-        n_gens += 1
     
     # Evento de recuperação
     else:
@@ -75,66 +76,57 @@ def run_epidemy(N, infection_rate, recovery_rate):
       n -= 1
       n_recovered += 1
 
-      # Enquanto um individuo estiver em recuperação, este vai gerar novas
-      # infecções, que serão os seus filhos e farão parte da mesma geração. Na
-      # medida que essas novas infecções são geradas, elas são atendidas
-      # imediatamente. Quando o tempo de recuperação do individuo atual for
-      # maior que o tempo de chegada da nova infecção, a geração atual é
-      # finalizada (ou seja, todos os individuos gerados, já foram atendidos/
-      # recuperados) e o processo de geração de novas infecções é reiniciado
-      offspring = []
-      t_child_next_infection = 0
-      t_child_next_recovery = 0
-      t_child_lambda = 0
-      t_child_mu = 0
-      tot_children_lambda = 0
-      tot_children_mu = 0
-      while True:
-        # Filhos do individuo que está em recuperação. Cada elemento da lista é
-        # o clock na chegada de um novo individuo
-        elapsed_time = 0
+      # Troca de geração
+      if n_offspring == n_recovered:
+        if n_gens not in children:
+          children[n_gens] = []
+        n_gens += 1
+        n_offspring = 0
 
-        # Gera uma nova infecção/chegada
-        rand = random.random()
-        t_child_lambda = -math.log(rand) / infection_rate
-        t_child_next_infection = clock + t_child_lambda
-        tot_children_lambda += t_child_lambda
-        elapsed_time += t_child_lambda
+      # Enquanto um individuo estiver em recuperação, este vai gerar novas
+      # infecções, que serão os seus filhos e farão parte da mesma geração.
+      # Quando o tempo de recuperação do individuo atual for maior que o
+      # acumulado dos tempos de chegadas da nova infecção, a geração de
+      # filhos deste individuos encerra
+      elapsed_time = 0
+      n_children = 0
+      while elapsed_time < t_mu:
+        n_children += 1
 
         # Atualiza relogio e contadores
-        clock = t_child_next_infection
         n += 1
         n_infected += 1
 
-        # Adiciona o tempo de chegada do novo individuo na lista de filhos
-        offspring.append(clock)
-
-        # Gera tempo de recuperação
+        # Gera uma infecção/filho
         rand = random.random()
-        t_child_mu = -math.log(rand) / recovery_rate
-        t_child_next_recovery = clock + t_child_mu
-        tot_children_mu += t_child_mu
-            
-        # Atualiza relogio e contadores
-        clock = t_child_next_recovery
-        n -= 1
-        n_recovered += 1
+        t_lambda = -math.log(rand) / infection_rate
+        t_next_infection = clock + t_lambda
+        tot_lambda += t_lambda
 
-        
-        # Se o somatorio das chegadas dos filhos for maior que o tempo de
-        # recuperação do individuo atual, então a geração atual é finalizada
-        # e o processo de geração de novas infecções é reiniciado Enquanto isso
-        # não acontece, novas infecções são geradas e atendidas imediatamente
-        if elapsed_time < t_mu:
-          # Adiciona a geração atual no dicionário de gerações
-          generations[n_gens] = offspring
-          n_gens += 1
-          # Atualiza tempos de infecção e recuperação
-          t_next_infection += (tot_children_lambda + tot_children_mu)
-          t_next_recovery += (tot_children_lambda + tot_children_mu)
-          tot_lambda += tot_children_lambda
-          tot_mu += tot_children_mu
-          break
+        if n_gens not in generations:
+          generations[n_gens] = []
+        generations[n_gens].append(t_next_infection)
+
+        n_offspring += 1
+        elapsed_time += t_lambda
+      
+      # Se o indiduo atual não gerou nenhum filho, adiciona 0 ao dicionario
+      if n_gens not in children:
+        children[n_gens] = []
+      children[n_gens].append(n_children)
+
+      # Atualiza relogio com o tempo de infecção do ultimo filho gerado
+      clock = t_next_infection
+
+      # Gera proxima recuperação/saída ou, se não houver mais indivíduos no
+      # sistema, força proximo evento ser uma infecção/chegada
+      if n == 0:
+        t_next_recovery = INF
+      else:
+        rand = random.random()
+        t_mu = -math.log(rand) / recovery_rate
+        t_next_recovery = clock + t_mu
+        tot_mu += t_mu
 
     # Se o numero de individuos no sistema for zero e a geração anterior não tiver
     # nenhum individuo, então a epidemia acabou
@@ -144,15 +136,18 @@ def run_epidemy(N, infection_rate, recovery_rate):
 
   return {
     "clock": clock, # Tempo final de simulacao
-    "generations": generations, # Dicionario de geracoes
+    "generations": generations, # Dicionario de geracoes,
+    "children": children, # Dicionario de filhos por geracao
   }
 
 def main():
-  N = 4
+  N = 2
   infection_rate = 1
-  recovery_rate = 0.5
+  recovery_rate = 20
   result = run_epidemy(N, infection_rate, recovery_rate)
   print('Gerações: ', result["generations"])
+  print('Filhos: ', result["children"])
+  print('Tempo final de simulação:', result["clock"])
 
 if __name__ == "__main__":
   main()
