ddf4cb813207117abf0ae2479b9fcc0e7e051043
naccaratocarolina <naccaratocarolina@dcc.ufrj.br>
2023-11-17 20:39:57 -0300

    rsrsrsrsrs

--- T2/run_cases.py
+++ T2/run_cases.py
@@ -5,8 +5,8 @@ from analisys import run_analisys
 # Caso 1: 
 infinita_caso1 = mm1_epidemy(
   num_sims=20,
-  tx_chegada=2,
-  tx_saida=1,
+  tx_chegada=1,
+  tx_saida=2,
   name='caso1',
   infinita=True,
 )
@@ -14,8 +14,8 @@ infinita_caso1 = mm1_epidemy(
 # Caso 2:
 infinita_caso2 = mm1_epidemy(
   num_sims=20,
-  tx_chegada=4,
-  tx_saida=2,
+  tx_chegada=2,
+  tx_saida=4,
   name='caso2',
   infinita=True,
 )
@@ -23,8 +23,8 @@ infinita_caso2 = mm1_epidemy(
 # Caso 3:
 infinita_caso3 = mm1_epidemy(
   num_sims=20,
-  tx_chegada=1,
-  tx_saida=1.05,
+  tx_chegada=1.05,
+  tx_saida=1,
   name='caso3',
   infinita=True,
 )
@@ -32,8 +32,8 @@ infinita_caso3 = mm1_epidemy(
 # Caso 4:
 infinita_caso4 = mm1_epidemy(
   num_sims=20,
-  tx_chegada=1,
-  tx_saida=1.10,
+  tx_chegada=1.10,
+  tx_saida=1,
   name='caso4',
   infinita=True,
 )
@@ -42,8 +42,8 @@ infinita_caso4 = mm1_epidemy(
 # Caso 1: 
 infinita_det_caso1 = mm1_epidemy(
   num_sims=20,
-  tx_chegada=2,
-  tx_saida=1,
+  tx_chegada=1,
+  tx_saida=2,
   name='caso1',
   infinita=True,
   deterministico=True,
@@ -52,8 +52,8 @@ infinita_det_caso1 = mm1_epidemy(
 # Caso 2:
 infinita_det_caso2 = mm1_epidemy(
   num_sims=20,
-  tx_chegada=4,
-  tx_saida=2,
+  tx_chegada=2,
+  tx_saida=4,
   name='caso2',
   infinita=True,
   deterministico=True,
@@ -62,8 +62,8 @@ infinita_det_caso2 = mm1_epidemy(
 # Caso 3:
 infinita_det_caso3 = mm1_epidemy(
   num_sims=20,
-  tx_chegada=1,
-  tx_saida=1.05,
+  tx_chegada=1.05,
+  tx_saida=1,
   name='caso3',
   infinita=True,
   deterministico=True,
@@ -72,8 +72,8 @@ infinita_det_caso3 = mm1_epidemy(
 # Caso 4:
 infinita_det_caso4 = mm1_epidemy(
   num_sims=20,
-  tx_chegada=1,
-  tx_saida=1.10,
+  tx_chegada=1.10,
+  tx_saida=1,
   name='caso4',
   infinita=True,
   deterministico=True,
@@ -82,69 +82,69 @@ infinita_det_caso4 = mm1_epidemy(
 ### Simulações Finitas Não Deterministicas ###
 # Caso 1: 
 finita_caso1 = mm1_epidemy(
-  num_sims=20,
-  tx_chegada=2,
-  tx_saida=1,
+  num_sims=100,
+  tx_chegada=1,
+  tx_saida=2,
   name='caso1',
 )
 
 # Caso 2:
 finita_caso2 = mm1_epidemy(
-  num_sims=20,
-  tx_chegada=4,
-  tx_saida=2,
+  num_sims=100,
+  tx_chegada=2,
+  tx_saida=4,
   name='caso2',
 )
 
 # Caso 3:
 finita_caso3 = mm1_epidemy(
-  num_sims=20,
-  tx_chegada=1,
-  tx_saida=1.05,
+  num_sims=100,
+  tx_chegada=1.05,
+  tx_saida=1,
   name='caso3',
 )
 
 # Caso 4:
 finita_caso4 = mm1_epidemy(
-  num_sims=20,
-  tx_chegada=1,
-  tx_saida=1.10,
+  num_sims=100,
+  tx_chegada=1.10,
+  tx_saida=1,
   name='caso4',
 )
 
 ### Simulações Finitas Deterministicas ###
 # Caso 1: 
 finita_det_caso1 = mm1_epidemy(
-  num_sims=20,
-  tx_chegada=2,
-  tx_saida=1,
+  num_sims=100,
+  tx_chegada=1,
+  tx_saida=2,
   name='caso1',
   deterministico=True,
 )
 
 # Caso 2:
 finita_det_caso2 = mm1_epidemy(
-  num_sims=20,
-  tx_chegada=4,
-  tx_saida=2,
+  num_sims=100,
+  tx_chegada=2,
+  tx_saida=4,
   name='caso2',
   deterministico=True,
 )
 
 # Caso 3:
 finita_det_caso3 = mm1_epidemy(
-  num_sims=20,
-  tx_chegada=1,
-  tx_saida=1.05,
+  num_sims=100,
+  tx_chegada=1.05,
+  tx_saida=1,
   name='caso3',
   deterministico=True,
 )
 
 # Caso 4:
 finita_det_caso4 = mm1_epidemy(
-  num_sims=20,
-  tx_chegada=1,
-  tx_saida=1.10,
+  num_sims=100,
+  tx_chegada=1.10,
+  tx_saida=1,
   name='caso4',
   deterministico=True,
 )
@@ -152,7 +152,7 @@ finita_det_caso4 = mm1_epidemy(
 # Roda as simulacoes
 
 # infinita_caso1.run_mm1_epid()
-infinita_caso2.run_mm1_epid()
+# infinita_caso2.run_mm1_epid()
 # infinita_caso3.run_mm1_epid()
 # infinita_caso4.run_mm1_epid()
 
@@ -161,14 +161,14 @@ infinita_caso2.run_mm1_epid()
 # infinita_det_caso3.run_mm1_epid()
 # infinita_det_caso4.run_mm1_epid()
 
-# finita_caso1.run_mm1_epid()
-# finita_caso2.run_mm1_epid()
-# finita_caso3.run_mm1_epid()
-# finita_caso4.run_mm1_epid()
+finita_caso1.run_mm1_epid()
+finita_caso2.run_mm1_epid()
+finita_caso3.run_mm1_epid()
+finita_caso4.run_mm1_epid()
 
-# finita_det_caso1.run_mm1_epid()
-# finita_det_caso2.run_mm1_epid()
-# finita_det_caso3.run_mm1_epid()
-# finita_det_caso4.run_mm1_epid()
+finita_det_caso1.run_mm1_epid()
+finita_det_caso2.run_mm1_epid()
+finita_det_caso3.run_mm1_epid()
+finita_det_caso4.run_mm1_epid()
 
 run_analisys()
